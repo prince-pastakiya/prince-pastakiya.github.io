e7f6b79e22c8d1773f340429662c9812c6e11028
Prince H. Pastakiya <93633708+prince-pastakiya@users.noreply.github.com>
2024-11-03 23:51:49 -0600

    Changing js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,30 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Photographer and Designer.", "Coder.", "Web Developer.", "Softwere Developer."],
-    typeSpeed: 100,
-    backSpeed: 100,
-    bachDelay: 10000,
-    loop: true
-})
-
-const header = document.querySelector("header");
-window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY > 100);
+	strings: [
+		"Photographer and Designer.",
+		"Coder.",
+		"Web Developer.",
+		"Software Developer.",
+	],
+	typeSpeed: 10,
+	backSpeed: 10,
+	bachDelay: 10,
+	loop: true,
 });
 
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+	header.classList.toggle("sticky", window.scrollY > 100);
+});
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
 
 menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navlist.classList.toggle('open');
+	menu.classList.toggle("bx-x");
+	navlist.classList.toggle("open");
 };
 
 window.onscroll = () => {
-	menu.classList.remove('bx-x');
-	navlist.classList.remove('open');
+	menu.classList.remove("bx-x");
+	navlist.classList.remove("open");
 };
